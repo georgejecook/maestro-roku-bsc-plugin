840f9da1a7f2b7d62cdf01bbb86ab7e7181827c2
George Cook <george@tantawowa.com>
2022-06-08 12:53:05 +0200

    --ammend

--- src/plugin.spec.ts
+++ src/plugin.spec.ts
@@ -2658,6 +2658,7 @@ describe('MaestroPlugin', () => {
                     print(asString(name, "default name"))
                     if asBoolean(isActive)
                         print asInteger(numSeconds, -1)
+                        print asNumber(numSeconds, -1)
                     end if
                     print m.items.getValue(asArray(items))
                     print m.items.show(asArray(items, ["first]))
@@ -2680,6 +2681,7 @@ describe('MaestroPlugin', () => {
                 print (mc_getString(name, invalid, "default name"))
                 if mc_getBoolean(isActive, invalid)
                     print mc_getInteger(numSeconds, invalid, - 1)
+                    print mc_getNumber(numSeconds, invalid, - 1)
                 end if
                 print m.items.getValue(mc_getArray(items, invalid))
             end function
